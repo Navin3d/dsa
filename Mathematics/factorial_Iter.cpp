3b24171e1589e92a8cfae38aaa925a49317c9ead
Ishani Saravanan <83009295+ishani1124@users.noreply.github.com>
2021-06-01 11:20:04 +0530

    Add files via upload

---
+++ Mathematics/factorial_Iter.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int fact(int n)
+{
+    int count = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        count = count * i;
+    }
+
+    return count;
+}
+
+int main()
+{
+    cout << "Enter a number to find Factorial: ";
+    int n;
+    cin >> n;
+    cout << fact(n) << endl;
+}
