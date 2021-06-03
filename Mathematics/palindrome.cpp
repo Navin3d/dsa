815e4574a64f5f61e4c3947f590a5e026d6d928f
Ishani Saravanan <83009295+ishani1124@users.noreply.github.com>
2021-06-03 10:08:19 +0530

    Add files via upload

---
+++ Mathematics/palindrome.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+bool palindrome(int n)
+{
+    int temp = n;
+    int result = 0;
+    while (temp != 0)
+    {
+        int lastdigit = temp % 10;
+        result = result * 10 + lastdigit;
+        temp = temp / 10;
+    }
+    return (n == result);
+}
+
+int main()
+{
+    cout << "Enter a number to check if it is palindrome or not: ";
+    int n;
+    cin >> n;
+    if (palindrome(n))
+        cout << "Palindrome" << endl;
+    else
+        cout << "Not a Palindrome" << endl;
+}
