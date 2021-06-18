9ae0bbc09fb2a21ee55e0d57c3bfd82ca37a0d83
Ishani Saravanan <83009295+ishani1124@users.noreply.github.com>
2021-06-18 16:20:47 +0530

    Create setbit2.cpp

---
+++ Bit Manipulation/setbit2.cpp
@@ -0,0 +1,22 @@
+//Brian and Kerningham Algorithm
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+unsigned int countSetBits(int n) 
+{ 
+       unsigned int count = 0; 
+       while (n) { 
+          n &= (n - 1); 
+          count++; 
+       } 
+      return count; 
+} 
+
+
+int main() 
+{ 
+	int i = 9; 
+	cout << countSetBits(i); 
+	return 0; 
+} 
