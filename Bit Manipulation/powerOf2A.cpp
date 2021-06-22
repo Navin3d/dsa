b56ce9512485846f4c41b26f28cdb46dfc5e1376
Ishani Saravanan <83009295+ishani1124@users.noreply.github.com>
2021-06-22 20:15:43 +0530

    Create powerOf2A.cpp

---
+++ Bit Manipulation/powerOf2A.cpp
@@ -0,0 +1,27 @@
+//NAIVE APPROACH TO FIND THE POWER TO 2
+
+#include <iostream>
+using namespace std;
+
+bool isPow2(int n)
+{
+    if(n == 0)
+        return true;
+    while(n != 1)
+    {
+        if(n % 2 != 0)
+            return false;
+        n = n / 2;
+    }
+    return true;
+}
+
+
+int main() {
+	
+	int n ;
+	cout<<"Enter a number: ";
+	cin>>n;
+	
+	printf("%s", isPow2(n)? "true": "false");
+}
