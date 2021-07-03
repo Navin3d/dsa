d2830aea79a533369f0f895a892b789c7cc50480
Ishani Saravanan <83009295+ishani1124@users.noreply.github.com>
2021-07-03 20:00:45 +0530

    Update oneMissingArr.cpp

--- Bit Manipulation/oneMissingArr.cpp
+++ Bit Manipulation/oneMissingArr.cpp
@@ -6,13 +6,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int getMissingNo(int a[], int n)
+int getMissingNo(int arr[], int n)
 {
-	int x1 = a[0];
+	int x1 = arr[0];
 	int x2 = 1;
 
 	for (int i = 1; i < n; i++)
-		x1 = x1 ^ a[i];
+		x1 = x1 ^ arr[i];
 
 	for (int i = 2; i <= n + 1; i++)
 		x2 = x2 ^ i;
