ae5e4b6ecb378603d7c5aa7be9393c22301b299c
Ishani Saravanan <83009295+ishani1124@users.noreply.github.com>
2021-07-14 22:33:07 +0530

    Create oddEven.cpp

---
+++ Bit Manipulation/oddEven.cpp
@@ -0,0 +1,17 @@
+// The LSD of odd number is 1   
+// The LSD of even number is 0 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int n;
+   cin>>n;
+   if((n&1) == 1)
+        cout<<"ODD";
+   else
+      cout<<"EVEN";
+      
+    return 0;
+}
