6c20a0d4df3a05d6eae8d94b4863e94c7a5f08af
Ishani Saravanan <83009295+ishani1124@users.noreply.github.com>
2021-07-21 13:20:33 +0530

    Create isSort.cpp

---
+++ Array/isSort.cpp
@@ -0,0 +1,22 @@
+//O(n)
+#include <iostream>
+using namespace std;
+
+bool isSort(int a[], int n){
+    for(int i=1;i<n;i++){
+        if(a[i]<a[i-1])
+          return false;
+    }
+    return true;
+}
+
+int main()
+{
+    int a[] = {1,7,9,12,34};
+    int size = sizeof(a)/sizeof(a[0]);
+    if(isSort(a,size))
+       cout<<"Sorted";
+    else
+      cout<<"Not sorted";
+    return 0;
+}
