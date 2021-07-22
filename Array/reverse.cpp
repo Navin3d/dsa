54d072319fd5076b4e053f29262ae1804e31bbc8
Ishani Saravanan <83009295+ishani1124@users.noreply.github.com>
2021-07-22 22:16:50 +0530

    Create reverse.cpp

---
+++ Array/reverse.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void reverse(int a[] , int n){
+    int start =0 , end= n-1;
+    while(start<end){
+        int temp =a[start];
+         a[start] = a[end];
+         a[end] = temp;
+         end--;
+         start++;
+    }
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+}
+int main()
+{
+    int a[] = {1,2,3,4,5};
+    int size = sizeof(a)/sizeof(a[0]);
+    reverse(a,size);
+    return 0;
+}
