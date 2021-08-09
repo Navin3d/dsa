a4cc9e3894999a692cd3020fbeb973078f8ad480
Ishani Saravanan <83009295+ishani1124@users.noreply.github.com>
2021-08-09 21:32:49 +0530

    Create bubblesort.cpp

---
+++ Array/bubblesort.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void bubble_sort(int a[] , int n){
+    for(int i=1;i<n;i++){
+        for(int j=0 ; j<(n-i-1);j++){
+            if(a[j]>a[j-1])
+               swap(a[j] , a[j-1]);
+        }
+    }
+}
+
+int main()
+{
+   
+    int a[] = {1,5,7,9,12,17,24};
+    int size = sizeof(a)/sizeof(a[0]);
+    int key = 5;
+    bubble_sort(a,size);
+   for(int i=0;i<size;i++)
+     cout<<a[i]<<" ";
+    return 0;
+}
