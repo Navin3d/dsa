9f8b9256a8992bb7953164d7a7176c616338bec9
Ishani Saravanan <83009295+ishani1124@users.noreply.github.com>
2021-08-10 19:41:54 +0530

    Update bubblesort.cpp

--- Array/bubblesort.cpp
+++ Array/bubblesort.cpp
@@ -4,8 +4,8 @@ using namespace std;
 void bubble_sort(int a[] , int n){
     for(int i=1;i<n;i++){
         for(int j=0 ; j<(n-i-1);j++){
-            if(a[j]>a[j-1])
-               swap(a[j] , a[j-1]);
+            if(a[j]>a[j+1])
+               swap(a[j] , a[j+1]);
         }
     }
 }
