f17aef46cb9519e7c0d35d8fdac6395a1793c1bb
Ishani Saravanan <83009295+ishani1124@users.noreply.github.com>
2021-08-23 15:13:07 +0530

    Create largestString.cpp

---
+++ String/largestString.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include<cstring>
+using namespace std;
+
+int main()
+{
+    char a[1000];
+    char largest[1000];
+    int largest_len = 0;
+    int len =0;
+    int n;
+    cin>>n;
+    cin.get();
+    for(int i=0 ; i<n;i++){
+        cin.getline(a,1000);
+        int len = strlen(a);
+        if(len>largest_len){
+            largest_len = len;
+            strcpy(largest, a);
+        }
+    }
+    
+    cout<<largest<<" "<<largest_len;
+    return 0;
+}
