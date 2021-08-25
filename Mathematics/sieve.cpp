2af22b8587ca6dcd2fb9c79c8b4365246fa07e40
Ishani Saravanan <83009295+ishani1124@users.noreply.github.com>
2021-08-25 21:44:20 +0530

    Update sieve.cpp

--- Mathematics/sieve.cpp
+++ Mathematics/sieve.cpp
@@ -5,9 +5,9 @@ bool sieve(int n){
     bool a[100000];
     for(int i=2 ; i<=n ; i++)
        a[i] =  true;
-    for(int i = 2 ; i*i<=n ;i++){
+    for(int i = 2 ; i*i<=n ;i++){         // Factors occur in pairs
         if(a[i] == true){
-            for(int j = i*i; j<=n; j= j+i){
+            for(int j = i*i; j<=n; j= j+i){  //j=i*i becouse before i*i all the non-primes are markes as false by the previous prime number
                 a[j] = false;
             }
         }
