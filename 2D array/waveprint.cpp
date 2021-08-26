d009431335c5b2df8aa5c0d2f07af3de80c969d5
Ishani Saravanan <83009295+ishani1124@users.noreply.github.com>
2021-08-26 23:40:32 +0530

    Add files via upload

---
+++ 2D array/waveprint.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[1000][1000];
+    int row,col,val = 1;
+    cout<<"Enter rows and coloumns: ";
+    cin>>row>>col;
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            cin>>a[i][j];
+        }
+    }
+    
+    // wave print
+    for(int j=0;j<col ; j++){
+      // check if j is even or odd
+        if(j%2 == 0){    // even
+          for(int i=0;i<row ; i++)
+             cout<<a[row][col]<<" ";
+        }
+        else {         // odd
+            for(int i=row-1 ; i>=0 ;i--)
+                cout<<a[row][col]<<" ";
+        }
+    }
+   
+    return 0;
+}
