a0fb2730e16605c1fb6ec884724680df6aa313b5
Ishani Saravanan <83009295+ishani1124@users.noreply.github.com>
2021-09-01 22:06:56 +0530

    Create maxSubarraySum1.cpp

---
+++ Array/maxSubarraySum1.cpp
@@ -0,0 +1,35 @@
+//naive 
+//o(n3)
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a[] = {-4,1,3,-2,6,2,-8,-9,4};
+  int n=sizeof(a)/sizeof(a[0]);  //9
+
+  int currentSum=0;
+  int maxSum=0;
+  //left and right is used to print maximum subarray
+  int left=-1;
+  int right = -1;
+  int i=0,j=0,k=0;
+  for(i=0;i<n;i++){
+    for(j=i;j<n;j++){
+      currentSum=0;
+      for(k=i;k<=j;k++){
+        currentSum += a[k];
+      }
+        if(currentSum>maxSum){
+          maxSum= currentSum;
+          left=i;
+          right=j;
+        }
+      }
+    }
+
+  cout<<maxSum<<endl;
+  for(int sub=left;sub<=right;sub++){
+    cout<<a[sub]<<" ";
+  }
+
+}
