2e64308b654fe81098c405d35a623baaf5401076
Ishani Saravanan <83009295+ishani1124@users.noreply.github.com>
2021-09-09 23:03:23 +0530

    Add files via upload

---
+++ Array/rotateOne.cpp
@@ -0,0 +1,20 @@
+//left rotate by 1 (counter-clockwise)
+#include <iostream>
+using namespace std;
+
+void rotateOne(int a[], int n){
+	int temp= a[0];
+	for(int i=1;i<n;i++){
+		a[i-1] = a[i];
+	}
+	a[n-1]=temp;
+}
+
+int main(){
+
+	int a[] = {2,6,8,7,9,10};
+	int n=sizeof(a)/sizeof(a[0]); 
+    rotateOne(a,n);
+    for(int i=0;i<n;i++)
+    	 cout<<a[i]<<" ";
+}
