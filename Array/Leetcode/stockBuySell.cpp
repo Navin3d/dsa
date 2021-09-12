3fc732d23ca005eb1f04cf843eca26e7bf8ad1e7
Ishani Saravanan <83009295+ishani1124@users.noreply.github.com>
2021-09-12 23:13:37 +0530

    Add files via upload

---
+++ Array/Leetcode/stockBuySell.cpp
@@ -0,0 +1,18 @@
+//Leetcode 121
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+        int minValue = INT_MAX;
+            
+            for(int i=0; i<prices.size() ; i++){
+                profit = max(profit, prices[i] - minValue );
+                minValue = min(minValue, prices[i]);
+            }
+        
+         return profit;
+    }
+};
