f9c09199997f38dbe0ff84ff4e1553fee53f23af
Ishani Saravanan <ishanisaravanan@gmail.com>
2021-11-14 20:17:07 +0530

    add Binary Search STL

--- Search/BinarySearch/binarySTL.cpp
+++ Search/BinarySearch/binarySTL.cpp
@@ -4,14 +4,29 @@ using namespace std;
 
 int main()
 {
-  int a[] = {1,2,3,4,5,6};
+  int a[] = {1,2,2,2,3,3,4,5,6};
   int n = sizeof(a)/sizeof(a[0]);
   int key;
   cin>>key;
 
   bool isPresent = binary_search(a,a+n, key);
   if(isPresent)
-     cout<<"PRESENT";
+     cout<<"PRESENT"<<endl;
   else
-      cout<<"ABSENT";
+      cout<<"ABSENT"<<endl;
+       
+      //first occurance of target 
+      auto first = lower_bound(a, a+n, 3);    //last is address of the first occurance
+      //address of first occurance - address of starting element in array
+      cout<<(first-a)<<endl;   //4
+
+      //next occurance of target
+      auto last = upper_bound(a, a+n ,3);  //last is address of the last occurance
+          //address of last occurance - address of starting element in array
+      cout<< (last-a) <<endl;  //6
+
+      //frequency of the target element can be found by upper_bound and lower_bound
+      cout<<(last- first)<<endl;      //2
+
+
 }
