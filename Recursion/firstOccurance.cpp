feb427dd39769900d50babe64f43bc477e54257e
Ishani Saravanan <ishanisaravanan@gmail.com>
2022-01-06 16:14:41 +0530

    update firstOccurance.cpp

--- Recursion/firstOccurance.cpp
+++ Recursion/firstOccurance.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int findFirst(int a[], int n, int key){
+int firstOccurance(int a[], int n, int key){
     if(n==0)
       return -1;
     if(a[0] == key)
         return 0;
     
-    int subindex = findFirst(a+1, n-1, key);
+    int subindex = firstOccurance(a+1, n-1, key);
     if(subindex != -1)
        return subindex+1;
        
@@ -21,7 +21,7 @@ int main()
     int n= sizeof(a)/sizeof(a[0]);
     int key;
     cin>>key;
-   cout<< findFirst(a,n,key);
+   cout<< firstOccurance(a,n,key);
     
     return 0;
 }
