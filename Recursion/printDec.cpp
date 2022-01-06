ecaac55da3f126ba82f000dd18da16dc85ce9685
Ishani Saravanan <ishanisaravanan@gmail.com>
2022-01-06 13:11:51 +0530

    added printDec.cpp in recursion

---
+++ Recursion/printDec.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+
+void printDec(int n)
+{
+   //base case
+    if(n==0)
+        return;
+    cout<<n<<endl; 
+    printDec(n-1);   //recursive case
+   
+}
+
+int main()
+{
+    
+    int n;
+    cin>>n;
+    printDec(n);
+    return 0;
+}
